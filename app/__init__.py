6084ff7f89a3cbaee13ea424240c32bbba8374d2
Owais Mustafa <owaismustafa@Owaiss-MacBook-Air.local>
2026-02-09 14:39:34 +0000

    added models, unfinished

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,18 @@
 # Initialise Flask here
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
+
+db = SQLAlchemy(app)
+
+if __name__ == "__main__":
+    
+    app.run(debug=True)
+app.app_context()
+
 
 @app.route('/')
 def index():
--- app/models.py
+++ app/models.py
@@ -1,1 +1,33 @@
 # Write SQLAlchemy class models here
+from __init__ import db
+
+class users(db.model):
+    user_id = db.Column("user_id", db.Integer, primary_key = True) #PK
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(50))
+    avatar = db.Column(db.String(100))
+    username = db.Column(db.String(20))
+    
+class Friends(db.model):
+    _id = db.Column("id", db.Integer, primary_key = True) #PK
+    user_id = db.Column(db.Integer, db.ForeignKey('users'.id))
+    friend_id = db.Column(db.Integer)
+
+class Partnership(db.model):
+    _id = db.Column("id", db.Integer, primary_key = True)#PK
+    partner_id = db.Column("id", db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users'.id))
+
+class UserHabits(db.model):
+    _id = db.Column("id", db.Integer, primary_key = True) #PK
+    user_id = db.Column(db.Integer, db.ForeignKey('users'.id)) #FK
+    habit_id = db.Column(db.Integer, db.ForeignKey('habits'.id)) #FK
+    frequency = db.Column(db.Integer)
+    progress_number = db.Column(db.Integer)
+    completed = db.Column(db.Boolean)
+    completion_date = db.Column(db.String)
+
+class Habits(db.model):
+    habit_id = db.Column(db.Integer, primary_key = True)#PK
+    habit_name = db.Column(db.String)
+    goal = db.Column(db.String)
