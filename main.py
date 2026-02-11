41f9acfbdf2c311161285fdf14fec0fa3eb68057
DanieruG <danielgyabaah952@outlook.com>
2026-02-11 22:18:34 +0000

    Bad file

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 # Run this to run the Flask application, by importing the function from __init__.py
+from app import createApp
+
+app = createApp()
+
+if __name__ == '__main__':
+    app.run(debug=True)
