e6184b53015376dc10159ce506e7f96f05b8fe8a
Owais Mustafa <owaismustafa@Owaiss-MacBook-Air.local>
2026-02-28 07:07:30 +0000

    random commit

--- app/streak.py
+++ app/streak.py
@@ -16,9 +16,9 @@ def update_streak(habit_id):
         habit.streak = habit.streak + 1
     db.session.commit()
 
-@repeat(every(1).minute) #every day at 1 am function runs
-def refresh_daily_habits(habit_id):
-    for habit in UserHabits:
-        if habit.daily and habit.completed == False: # if a daily habit and not complete reset streak
-            habit.streak = 0 
-    db.session.commit()
+    @repeat(every(1).minute) #every day at 1 am function runs
+    def refresh_daily_habits(habit_id):
+        for habit in UserHabits:
+            if habit.daily and habit.completed == False: # if a daily habit and not complete reset streak
+                habit.streak = 0 
+        db.session.commit()
